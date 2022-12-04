40e40eee59083df4623b5dc6316fa1c5cbcd0d76
EXP <289065406@qq.com>
2022-12-04 12:16:32 +0800

    脚本优化

---
+++ onekey.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+
+./modules/apt.sh
+./modules/python.sh
+./modules/env.sh
+./modules/vm.sh
+
+
+
+
+
+
+
+select options:
+
+1. install language
+2. set vm
+3. install ohmyzsh
+....
+
+生成 ssh-keygen
+
+
+配置 ssh 免密
+
