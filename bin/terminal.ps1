1083afa2504bf6344ea5207c4eaa3b71f6a684dc
EXP <289065406@qq.com>
2022-12-04 21:36:03 +0800

    支持 zsh 进入测试容器

--- bin/terminal.ps1
+++ bin/terminal.ps1
@@ -1,15 +1,24 @@
 # PowerShell
 #------------------------------------------------
 # 进入容器的交互终端
-# bin/terminal.ps1 
+# bin/terminal.ps1 [-s ${SHELL}]
 #------------------------------------------------
+
+param(
+    [string]$s="bash"
+)
+
+$SHELL = "zsh"
+if(![String]::IsNullOrEmpty($s)) {
+    $SHELL = $s
+}
 
 
 $CONTAINER_NAME = "SYS_FOR_TEST"
 $CONTAINER_ID = (docker ps -aq --filter name="$CONTAINER_NAME")
 if([String]::IsNullOrEmpty($CONTAINER_ID)) {
-    Write-Host "[$CONTAINER_NAME] 容器没有运行 ..."
+    Write-Host "[$CONTAINER_NAME] is not running ..."
 
 } else {
-    docker exec -it -u root $CONTAINER_ID bash
+    docker exec -it -u root $CONTAINER_ID $SHELL
 }
--- bin/terminal.sh
+++ bin/terminal.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 #------------------------------------------------
 # 进入容器的交互终端
-# bin/terminal.sh 
+# bin/terminal.sh [-s ${SHELL}]
 #------------------------------------------------
+
+SHELL="bash"
+
+set -- `getopt s: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) SHELL="$2"
+        shift ;;
+  esac
+  shift
+done
 
 
 CONTAINER_NAME="SYS_FOR_TEST"
 CONTAINER_ID=`docker ps -aq --filter name="$CONTAINER_NAME"`
 if [[ "${CONTAINER_ID}x" = "x" ]] ; then
-    echo "[$CONTAINER_NAME] 容器没有运行 ..."
+    echo "[$CONTAINER_NAME] is not running ..."
 else
-    docker exec -it -u root $CONTAINER_ID bash
+    docker exec -it -u root $CONTAINER_ID $SHELL
 fi
