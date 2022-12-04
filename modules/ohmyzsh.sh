fdd20e583d58dbc8dba5369faae453b6e3951cee
EXP <289065406@qq.com>
2022-12-04 21:35:30 +0800

    安装 ohmyzsh

--- modules/ohmyzsh.sh
+++ modules/ohmyzsh.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
+#----------------------------
 
-https://www.jianshu.com/p/ba782b57ae96
+MODULE="ohmyzsh"
+echo "+++++++++++++++++++++++++++++++"
+echo "SETTING [${MODULE}]"
+
+
+echo "Install zsh ..."
+if [[ ! "$SHELL" = "/bin/zsh" ]]; then
+    apt-get install -y zsh
+    chsh -s /bin/zsh 
+fi
+
+
+echo "Install oh my zsh ..."
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' /root/.zshrc
+sources /root/.zshrc
+
+
+echo "Add oh my zsh To all users ..."
+for user in `ls /home` 
+do
+    if [ ! -f "/home/${user}/.zshrc" ]; then
+        echo "Set oh my zsh For ${user} ..."
+        cp -R /root/.oh-my-zsh "/home/${user}/"
+        cp /root/.zshrc "/home/${user}/.zshrc"
+
+        chown ${user}:${user} "/home/${user}/.oh-my-zsh"
+        chown ${user}:${user} "/home/${user}/.zshrc"
+        chsh -s /bin/zsh ${user}
+    fi
+done
+
+
+echo "Done ."
+echo "-------------------------------"
+
