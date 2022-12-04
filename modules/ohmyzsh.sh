31a00dae49d6e1e44490d0fcb2ac38b338dde668
EXP <289065406@qq.com>
2022-12-04 21:44:30 +0800

    更改为国内源

--- modules/ohmyzsh.sh
+++ modules/ohmyzsh.sh
@@ -14,12 +14,13 @@ fi
 
 
 echo "Install oh my zsh ..."
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh -O -)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' /root/.zshrc
 sources /root/.zshrc
 
 
-echo "Add oh my zsh To all users ..."
+echo "Add oh my zsh For all users ..."
 for user in `ls /home` 
 do
     if [ ! -f "/home/${user}/.zshrc" ]; then
