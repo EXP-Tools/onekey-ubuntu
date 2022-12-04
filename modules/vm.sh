043549c755b9b797dc0454b4cd47b741408d1adc
EXP <289065406@qq.com>
2022-12-04 22:29:11 +0800

    Update vm.sh

--- modules/vm.sh
+++ modules/vm.sh
@@ -5,14 +5,15 @@ MODULE="virtual memory"
 echo "+++++++++++++++++++++++++++++++"
 echo "SETTING [${MODULE}]"
 
+VM_SIZE=4096
+VM_DIR="/swap"
+VM_FILE="swapfile"
+VM_PATH="${VM_DIR}/${VM_FILE}"
+
 
 echo "Set vm (4G) ..."
 vm_total=`free -mh | grep Swap | awk '{print $2}'`
 if [[ "x{$vm_total}" = "x0" ]]; then
-    VM_SIZE=4096
-    VM_DIR="/swap"
-    VM_FILE="swapfile"
-    VM_PATH="${VM_DIR}/${VM_FILE}"
     mkdir -p ${VM_DIR}
     dd if=/dev/zero of=${VM_PATH} bs=1M count=${VM_SIZE}
     chmod 600 ${VM_PATH}
