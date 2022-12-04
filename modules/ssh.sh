30dfaf7ed49345ec7517b1459c1a4db729d63917
EXP <289065406@qq.com>
2022-12-04 21:35:41 +0800

    ssh 免密

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
+#----------------------------
+
+MODULE="ssh"
+echo "+++++++++++++++++++++++++++++++"
+echo "SETTING [${MODULE}]"
 
 
-生成 ssh-keygen
-配置 ssh 免密
+SSHD_PATH="/etc/ssh/sshd_config"
+PUKK="PubkeyAuthentication"
+PASS="PasswordAuthentication"
+PUKK_RGX="^${PUKK}.*"
+PASS_RGX="^${PASS}.*"
+
+
+echo "Enable pubkey ..."
+grep "${PUKK_RGX}" ${SSHD_PATH}
+if [[ $? = 0 ]] ; then
+    sed -i "s/${PUKK_RGX}/${PUKK} yes/g" ${SSHD_PATH}
+else
+    echo "${PUKK} yes" >> ${SSHD_PATH}
+fi
+service sshd restart
+
+
+echo "Disable password ..."
+grep "${PASS_RGX}" ${SSHD_PATH}
+if [[ $? = 0 ]] ; then
+    sed -i "s/${PASS_RGX}/${PASS} no/g" ${SSHD_PATH}
+else
+    echo "${PASS} no" >> ${SSHD_PATH}
+fi
+service sshd restart
+
+
+echo "Done ."
+echo "-------------------------------"
