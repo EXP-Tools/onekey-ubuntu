c84c7071e4a8a3386bec5fcee999164475beb884
EXP <289065406@qq.com>
2022-12-06 00:32:06 +0800

    默认不关闭密码登录模式，避免未设置 public key 导致无法登录

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -23,14 +23,15 @@ fi
 service sshd restart
 
 
-echo "Disable password ..."
-grep "${PASS_RGX}" ${SSHD_PATH}
-if [[ $? = 0 ]] ; then
-    sed -i "s/${PASS_RGX}/${PASS} no/g" ${SSHD_PATH}
-else
-    echo "${PASS} no" >> ${SSHD_PATH}
-fi
-service sshd restart
+# 默认不关闭密码登录模式，避免未设置 public key 导致无法登录
+# echo "Disable password ..."
+# grep "${PASS_RGX}" ${SSHD_PATH}
+# if [[ $? = 0 ]] ; then
+#     sed -i "s/${PASS_RGX}/${PASS} no/g" ${SSHD_PATH}
+# else
+#     echo "${PASS} no" >> ${SSHD_PATH}
+# fi
+# service sshd restart
 
 
 echo "Done ."
