ceddb2060d58d8853b46d1d9463386ea2804876a
EXP <289065406@qq.com>
2022-12-05 13:24:16 +0800

    脚本优化

--- modules/env.sh
+++ modules/env.sh
@@ -7,37 +7,43 @@ echo "SETTING [${MODULE}]"
 
 
 echo "Set system timezone ..."
-echo "export TZ=Asia/Shanghai" >> /etc/profile
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-apt-get install tzdata
+grep "Shanghai" /etc/profile
+if [[ ! $? = 0 ]] ; then
+    echo "export TZ=Asia/Shanghai" >> /etc/profile
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    apt-get install tzdata
+fi
 
 
 echo "Set UTF-8 chinese language ..."
-apt-get install -y \
-    language-pack-zh-hans \
-    language-pack-zh-hans-base \
-    language-pack-gnome-zh-hans \
-    language-pack-gnome-zh-hans-base
-apt-get install -y locales
-echo 'zh_CN.GBK GBK' >> /etc/locale.gen
-echo 'zh_CN.UTF-8 UTF-8' >> /etc/locale.gen
-echo "LANG=zh_CN.UTF-8" >> /etc/environment
-locale-gen
+grep "zh_CN" /etc/environment
+if [[ ! $? = 0 ]] ; then
+    apt-get install -y \
+        language-pack-zh-hans \
+        language-pack-zh-hans-base \
+        language-pack-gnome-zh-hans \
+        language-pack-gnome-zh-hans-base
+    apt-get install -y locales
+    echo 'zh_CN.GBK GBK' >> /etc/locale.gen
+    echo 'zh_CN.UTF-8 UTF-8' >> /etc/locale.gen
+    echo "LANG=zh_CN.UTF-8" >> /etc/environment
+    locale-gen
+fi
 
 
 echo "Set alias ..."
-ALIAS_LL="alias ll='ls -al'"
+ALIAS_LL="alias ll='ls -alF'"
 
-grep "${ALIAS_LL}" /root/.bashrc
+grep "alias ll" /root/.bashrc
 if [[ ! $? = 0 ]] ; then
     echo "${ALIAS_LL}" >> /root/.bashrc
 fi
 
 for user in `ls /home` 
 do
-    grep "${ALIAS_LL}" /home/${user}/.bashrc
+    grep "alias ll" /home/${user}/.bashrc
     if [[ ! $? = 0 ]] ; then
-        echo "alias ll='ls -al'" >> /home/${user}/.bashrc
+        echo "${ALIAS_LL}" >> /home/${user}/.bashrc
     fi
 done
 
